b51644037ded0668eaa74d91acde9876528c3e65
fywlqlp <fywlq_lp@yeah.net>
2021-05-10 08:31:17 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/lp/wiki/controller/TestController.java
+++ src/main/java/com/lp/wiki/controller/TestController.java
@@ -1,14 +1,18 @@
 package com.lp.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:TEST}") //默认值：TEST
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
