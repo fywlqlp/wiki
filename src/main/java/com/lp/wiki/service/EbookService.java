3fe200573cef4a5398ad2012db888fbe498e349d
fywlqlp <fywlq_lp@yeah.net>
2021-05-13 08:03:14 +0800

    3-6 完成电子书列表查询接口

---
+++ src/main/java/com/lp/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.lp.wiki.service;
+
+import com.lp.wiki.domain.Ebook;
+import com.lp.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
+    }
+}
