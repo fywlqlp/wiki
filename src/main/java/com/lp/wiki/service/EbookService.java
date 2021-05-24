39f631c9cbe4733372455f75bfeb76aa27a25a89
fywlqlp <fywlq_lp@yeah.net>
2021-05-24 14:53:39 +0800

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/lp/wiki/service/EbookService.java
+++ src/main/java/com/lp/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.lp.wiki.domain.EbookExample;
 import com.lp.wiki.mapper.EbookMapper;
 import com.lp.wiki.req.EbookReq;
 import com.lp.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.lp.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,14 +22,16 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList = new ArrayList<>();
+        //List<EbookResp> respList = new ArrayList<>();
 
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
-
-        return respList;
+//        for (Ebook ebook : ebookList) {
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook, ebookResp);
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+        // 列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 }
