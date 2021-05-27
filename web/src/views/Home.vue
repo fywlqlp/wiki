54fd57076b2aa1447ee8351be15a4ceb4e253c6a
fywlqlp <fywlq_lp@yeah.net>
2021-05-27 16:17:48 +0800

    5-5 通过增加 --port 参数来修改启动端口

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -97,7 +97,7 @@ export default defineComponent({
     const ebooks1 = reactive({books:[]})
 
     onMounted(() => {
-      axios.get("http://localhost:8881/ebook/list").then((resp) => {
+      axios.get("/ebook/list").then((resp) => {
         console.log(resp);
         const data = resp.data
         ebooks.value = data.content
