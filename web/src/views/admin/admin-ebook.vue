bc8cfe77c1773ed22f1a4c4ae34e48b579a35e48
fywlqlp <fywlq_lp@yeah.net>
2021-05-31 17:10:48 +0800

    6-7 模态框显示电子书表单，编辑某一行时可以将该行数据显示到表单

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -19,7 +19,7 @@
         </template>
         <template v-slot:action="{ text, record }">
           <a-space size="small">
-            <a-button type="primary" @click="edit">
+            <a-button type="primary" @click="edit(record)">
               编辑
             </a-button>
               <a-button type="danger">
@@ -31,7 +31,23 @@
     </a-layout-content>
   </a-layout>
   <a-modal v-model:visible="modalVisible" title="电子书表单" @ok="handleModalOk" :confirm-loading="modalLoading">
-    <p>test</p>
+    <a-form :model="ebook" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+      <a-form-item label="封面">
+        <a-input v-model:value="ebook.cover" />
+      </a-form-item>
+      <a-form-item label="名称">
+        <a-input v-model:value="ebook.name" />
+      </a-form-item>
+      <a-form-item label="分类一">
+       <a-input v-model:value="ebook.category1Id" />
+      </a-form-item>
+      <a-form-item label="分类二">
+        <a-input v-model:value="ebook.category2Id" />
+      </a-form-item>
+      <a-form-item label="描述">
+        <a-input v-model:value="ebook.description" type="textarea" />
+      </a-form-item>
+    </a-form>
   </a-modal>
 </template>
 
@@ -123,6 +139,7 @@ export default defineComponent({
     };
 
     //表单
+    const ebook = ref({})
     const modalVisible = ref(false)
     const modalLoading = ref(false)
     const handleModalOk = () => {
@@ -135,8 +152,9 @@ export default defineComponent({
     /**
      * 编辑
      */
-    const edit = () => {
+    const edit = (record: any) => {
       modalVisible.value = true;
+      ebook.value = record
     }
 
     onMounted(() => {
@@ -155,7 +173,8 @@ export default defineComponent({
       edit,
       modalVisible,
       modalLoading,
-      handleModalOk
+      handleModalOk,
+      ebook
     }
   }
 });
