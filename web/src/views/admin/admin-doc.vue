ea3c28961a611164dfcf6e3f261ae6ae34ea95ea
fywlqlp <fywlq_lp@yeah.net>
2021-06-16 14:45:11 +0800

    8-9 让表格数默认展开

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -20,12 +20,14 @@
             </a-form>
           </p>
           <a-table
+              v-if="level1.length > 0"
               :columns="columns"
               :row-key="record => record.id"
               :data-source="level1"
               :loading="loading"
               :pagination="false"
               size="small"
+              :defaultExpandAllRows="true"
           >
             <template #name="{ text, record }">
               {{record.sort}} {{text}}
@@ -126,6 +128,7 @@ export default defineComponent({
       }
     ];
     const level1 = ref()
+    level1.value = []
 
     /**
      * 数据查询
