6b38c75bd9952f9741b1ba3b4ab2396d7fbe9a4b
fywlqlp <fywlq_lp@yeah.net>
2021-06-22 19:11:01 +0800

    9-6 修改的时候，不修改密码

--- src/main/java/com/lp/wiki/service/UserService.java
+++ src/main/java/com/lp/wiki/service/UserService.java
@@ -78,6 +78,7 @@ public class UserService {
         } else {
             //更新
             user.setLoginName(null);
+            user.setPassword(null);
             userMapper.updateByPrimaryKeySelective(user);
         }
     }
