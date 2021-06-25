2aa0bedd12c6cf3526c21acda0b41c964df700a9
fywlqlp <fywlq_lp@yeah.net>
2021-06-25 08:09:41 +0800

    9-9 使用vuex保存用户信息，方便其它组件获取登录信息

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,5 +1,21 @@
 <template>
   <a-layout-footer style="text-align: center">
-    LP电子书
+    LP电子书{{user.name}}
   </a-layout-footer>
 </template>
+<script lang="ts">
+  import { defineComponent, computed } from 'vue'
+  import store from "@/store";
+  export default defineComponent({
+    name: 'the-footer',
+    setup() {
+      const user = computed(() => {
+        return store.state.user
+      })
+
+      return {
+        user
+      }
+    }
+  })
+</script>
